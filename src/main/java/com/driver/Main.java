e5ec7f82a43a3fc2e2fc4b601be1637158533139
gupta <guptasanchit041@gmail.com>
2023-01-14 21:35:23 +0530

    g

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,6 +3,8 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         B a = new B();
+        a.function();
+        a.function1();
         System.out.println(a.function());
         System.out.println(a.function1());
     }
